7ed5c9a4a334f4d99fa308b67836a749af8d3ac3
khnguyen.607 <khnguyen.job@gmail.com>
2023-12-11 02:56:33 +0700

    upload more..

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -7,26 +7,44 @@ document.addEventListener('DOMContentLoaded', () => {
             div.innerHTML = data
             document.querySelector('.iq-top-navbar').innerHTML = div.querySelector('.iq-top-navbar').innerHTML
             document.querySelector('.iq-footer').innerHTML = div.querySelector('.iq-footer').innerHTML
-            // ACTIVE MENU 
-            if (window.location.href.includes('/dashboard.html')) document.querySelector('#iq-sidebar-toggle a[href="dashboard.html"]').parentNode.classList.add('active')
-            else if (window.location.href.includes('/index.html')) document.querySelector('#iq-sidebar-toggle a[href="index.html"]').parentNode.classList.add('active')
+            // // ACTIVE MENU 
+            // if (window.location.href.includes('/dashboard.html')) document.querySelector('#iq-sidebar-toggle a[href="dashboard.html"]').parentNode.classList.add('active')
+            // else if (window.location.href.includes('/index.html')) document.querySelector('#iq-sidebar-toggle a[href="index.html"]').parentNode.classList.add('active')
 
             fetch('../backend/index.php?controller=user&action=check')
                 .then(response => response.json())
                 .then(data => {
+                    if (window.location.href.includes('/index.html')) {
+                        if (data.role !== '0') {
+                            window.location.href = 'dashboard.html';
+                        }
+                    } else if (window.location.href.includes('/dashboard.html')) {
+                        if (data.role === '0') {
+                            window.location.href = 'index.html';
+                        }
+                    }
+
                     document.querySelector('.header__user').classList.remove('d-none')
                     document.querySelector('.header__user h6').textContent = data.name
                     switch (data.role) {
+                        case '0':
+                            document.querySelector('.profile__btn.d-none').classList.remove('d-none')
+                            break;
                         case '1':
                             document.querySelector('.header__user img').src = 'assets/images/user/librarian.png'
                             break;
                         case '2':
                             document.querySelector('.header__user img').src = 'assets/images/user/admin.jpg'
+                            if (window.location.href.includes('/dashboard.html')) {
+                                document.querySelector('#view-mgr__user').classList.remove('d-none')
+                                document.querySelector('#view-mgr__room').classList.remove('d-none')
+                            }
                             break;
                     }
                 })
-                .catch(error=> {
+                .catch(error => {
                     document.querySelector('.btn__login').classList.remove('d-none')
+                    window.location.href = 'auth-sign-in.html'
                 })
         })
 })
